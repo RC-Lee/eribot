72456bdeb6a40ec1b1042a315f5dc3b9b49cc4d8
rclee91 <rlee85@myseneca.ca>
2021-05-11 00:12:15 -0400

    Inital commit

---
+++ bot/func.py
@@ -0,0 +1,87 @@
+import random
+import json
+with open("./bannerData.json") as f:
+    data = json.load(f)
+ebanner = data["eventBanner"]
+
+def get4(data):
+    char = []
+    for item in data:
+        if(item["star"] == "four"):
+            char.append(item)
+    return char
+
+
+def get5(data):
+    char = []
+    for item in data:
+        if(item["star"] == "five"):
+            char.append(item)
+    return char
+
+
+def choose5(data):
+    char = get5(data)
+    if random.random() < 0.5:
+        for item in char:
+            if item["up"]:
+                return item
+    else:
+        return random.choice(char)
+
+
+def choose4(data):
+    items = get4(data)
+    if random.random() < 0.5:
+        upItems = []
+        for item in items:
+            if item["up"]:
+                upItems.append(item)
+        return random.choice(upItems)
+    else:
+        return random.choice(items)
+
+async def rollEvent(userData):
+    tData = userData
+    eRoll = tData["eventRoll"][-1]["r"] + 1
+    e5Pity = tData["event5Pity"] + 1
+    e4Pity = tData["event4Pity"] + 1
+    eventPromo = tData["eventPromo"]
+    item = {}
+
+    if(e5Pity == 90):
+        rTitle = 'Roll: '+ str(eRoll) + ' Congrats you rolled a 5 star'
+        item = choose5(ebanner)
+        e5Pity = 0
+        e4Pity = 0
+        # collection.update_one({"_id": userData["_id"]}, {"$set": {"event5Pity": 0, "event4Pity": 0, "eventRoll": eRoll}})
+    elif(e4Pity == 10):
+        rTitle = 'Roll: '+ str(eRoll) + ' Congrats you rolled a 4 star'
+        item = choose4(ebanner)
+        e4Pity = 0
+        # collection.update_one({"_id": message.author.id}, {"$set": {"event4Pity": 0, "eventRoll": eRoll}})
+    else:
+        from random import randint
+        value = randint(1, 1000)
+        if value <= 6:
+            rTitle = 'Roll: '+ str(eRoll) + ' Congrats you rolled a 5 star'
+            item = choose5(ebanner)
+            e5Pity = 0
+            e4Pity = 0
+            # collection.update_one({"_id": message.author.id}, {"$set": {"event5Pity": 0, "event4Pity": 0, "eventRoll": eRoll}})
+        elif value <= 57:
+            item = choose4(ebanner)
+            rTitle = 'Roll: '+ str(eRoll) + ' Congrats you rolled a 4 star'
+            e4Pity = 0
+            # collection.update_one({"_id": message.author.id}, {"$set": {"event4Pity": 0, "eventRoll": eRoll}})
+        else:
+            rTitle = 'Roll: '+ str(eRoll) + ' Boo you rolled a 3 star'
+            # collection.update_one({"_id": message.author.id}, {"$set": {"event5Pity": e5Pity, "event4Pity": e4Pity, "eventRoll": eRoll}})
+    tData["event5Pity"] = e5Pity
+    tData["event4Pity"] = e4Pity
+    if(item):
+        tData["eventRoll"].append({"r": eRoll, "rStar": item["star"], "rName": item["name"], "rUrl": item["imgUrl"]})
+    else:
+        tData["eventRoll"].append({"r": eRoll, "rStar": "three", "rName": "", "rUrl": ""})
+
+    return tData
---
+++ bot/main.py
@@ -0,0 +1,70 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+import pymongo
+from pymongo import MongoClient
+import func
+
+load_dotenv()
+client = discord.Client()
+TOKEN = os.getenv("DISCORD_TOKEN")
+MONGOURL = os.getenv("MONGO_URL")
+
+cluster = MongoClient(MONGOURL)
+db = cluster["DiscordData"]
+collection = db["UserData"]
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if not message.guild.me.permissions_in(message.channel).read_messages:
+        return
+
+    if not message.guild.me.permissions_in(message.channel).send_messages:
+        return
+    
+    if message.content == '$roll_event':
+        myquery = {"_id": message.author.id}
+        if(collection.count_documents(myquery) == 0):
+            post = {"_id": message.author.id, "name": message.author.name, "eventRoll": [{"r": 0, "rStar":"NA", "rName": "", "rUrl": ""}], "eiRoll": 0, "normalRoll": 0, "event4Pity": 0, "ei4Pity": 0, "normal4Pity": 0, "event5Pity": 0, "ei5Pity": 0, "normal5Pity": 0, "eventPromo": 0, }
+            collection.insert_one(post)
+        
+        user = collection.find(myquery)
+        tData = await func.rollEvent(user[0])
+        collection.update_one({"_id": tData["_id"]}, {"$set": {"eventRoll": tData["eventRoll"], "event4Pity": tData["event4Pity"], "event5Pity":tData["event5Pity"]}})
+
+        rTitle = "You rolled a " + tData["eventRoll"][-1]["rStar"] + " star"
+        embed = discord.Embed(title=rTitle, color=discord.Color.purple())
+        embed.set_author(name=message.author.name, icon_url = message.author.avatar_url)
+        if tData["eventRoll"][-1]["rUrl"] != "":
+            embed.set_image(url=tData["eventRoll"][-1]["rUrl"])
+        embed.set_footer(text="Roll #" + str(tData["eventRoll"][-1]["r"]))
+        await message.channel.send(embed=embed)
+
+    if message.content == '$list_event':
+        myquery = {"_id": message.author.id}
+        if(collection.count_documents(myquery) == 0):
+            await message.channel.send("You haven't rolled yet")
+        else:
+            user = collection.find(myquery)
+            embed = discord.Embed(title="Listing 4 and 5 star event rolls", color=discord.Color.red())
+            for roll in user[0]["eventRoll"]:
+                if(roll["rStar"] == "four" or roll["rStar"] == "five"):
+                    embed.add_field(name="Roll " + str(roll["r"]) +": ", value=roll["rName"], inline=False)
+            await message.channel.send(embed=embed)
+    
+    if message.content == '$info':
+        embed = discord.Embed(title="List of commands", color=discord.Color.green())
+        embed.add_field(name="$roll_event", value="Roll once on the event banner", inline=False)
+        embed.add_field(name="$list_event", value="List 4 and 5 star rolls from event banner", inline = False)
+        await message.channel.send(embed = embed)
+    
+    
+client.run(TOKEN)
